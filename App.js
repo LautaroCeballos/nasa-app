a40da6608b4cf9ffd4017e6d5a9160906e6782bb
LautaroCeballos <lautaroceballos2013@gmail.com>
2024-07-30 16:41:30 -0300

    NativeWind Config

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text>tailwindcss configurado</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
